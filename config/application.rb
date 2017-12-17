87d4d88025c19012e2785b962c6a47c9dbd9171f
Chi Tang V.Q <tvquocchi@gmail.com>
2017-12-17 15:07:53 +0700

    try fix cors again 2

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ module Blog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    use Rack::Cors do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: :any
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
 
