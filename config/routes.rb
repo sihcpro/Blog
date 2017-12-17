282eb38ee46606340e0fc428044f019b87b99e06
Chi Tang V.Q <tvquocchi@gmail.com>
2017-12-17 09:39:12 +0700

    add register to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :users
+  resource :users, only: %i[show update]
+  post '/register', to: 'users#create', as: 'register'
 end
